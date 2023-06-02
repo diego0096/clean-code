b3346db9badd216a60bdcbfe9a7b6cb13052d645
diego0096 <dfqz93@hotmail.com>
2023-06-01 23:09:34 -0500

    exercises

---
+++ examples.js
@@ -0,0 +1,26 @@
+// Arrays
+const numList = [1, 2, 3, 4, 5];
+const nameList = ["Cande", "Alex", "Mariana"];
+const colorsList = ["red", "green", "blue"];
+
+// Booleans
+const isActive = true;
+const isCorrect = false;
+const canWrite = true;
+const isUser = false;
+
+// Numbers
+let totalSum = 3543;
+const num = 20;
+const PI = 3.14159
+
+// Function
+sendAlert()
+createObject()
+setPassword()
+getResults()
+
+// Classes
+class School {}
+class Service {}
+class Account {}
