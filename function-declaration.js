56904b5de08b7da2fb369e69ae9d5d77bb9fff54
diego0096 <dfqz93@hotmail.com>
2023-06-04 21:56:58 -0500

    declaration

---
+++ function-declaration.js
@@ -0,0 +1,13 @@
+// Declaración de funciones
+function getResult() {
+    return "Results";
+}
+
+getResult();
+
+// Expresión de funciones
+const getResult = function() {
+    return "Results";
+}
+
+getResult();
